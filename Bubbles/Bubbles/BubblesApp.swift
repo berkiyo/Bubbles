fe3944444e240f8954a6dc986f97e76d11555e12
Berk Dogan <berk@tekbyte.net>
2023-05-28 01:49:02 +1000

    Import project data

---
+++ Bubbles/Bubbles/BubblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblesApp.swift
+//  Bubbles
+//
+//  Created by Berk Dogan on 28/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct BubblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
